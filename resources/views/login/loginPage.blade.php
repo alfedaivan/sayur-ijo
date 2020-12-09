9e34fd4b24a5113ab48e0d307c75bbf86b0b848a
alfeda ivan widiaputra <alfedaivan@gmail.com>
2020-12-09 14:00:01 +0700

    fixing login page

--- resources/views/login/loginPage.blade.php
+++ resources/views/login/loginPage.blade.php
@@ -1,142 +1,86 @@
-<!--
-author: W3layouts
-author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Grocery Store a Ecommerce Online Shopping Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
-<!-- for-mobile-apps -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Grocery Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
-		function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- //for-mobile-apps -->
-<link href="{{url('/assets/TemplateUser/css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all"/>
-<link href="{{url('/assets/TemplateUser/css/style.css')}}" rel="stylesheet" type="text/css" media="all"/>
-<!-- font-awesome icons -->
-<link href="{{url('/assets/TemplateUser/css/font-awesome.css')}}" rel="stylesheet" type="text/css" media="all"/>
-<!-- //font-awesome icons -->
-<!-- js -->
-<script src="{{url('/assets/TemplateUser/js/jquery-1.11.1.min.js')}}"></script>
-<!-- //js -->
-<link href='//fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
-<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
-<!-- start-smoth-scrolling -->
-<script type="text/javascript" src="{{url('/assets/TemplateUser/js/move-top.js')}}"></script>
+  <head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-<script type="text/javascript" src="{{url('/assets/TemplateUser/js/easing.js')}}"></script>
-<!-- start-smoth-scrolling -->
+  <title></title>
+
+  <!-- GOOGLE FONTS -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500" rel="stylesheet"/>
+  <link href="https://cdn.materialdesignicons.com/3.0.39/css/materialdesignicons.min.css" rel="stylesheet" />
+
+  <!-- PLUGINS CSS STYLE -->
+  <link href="{{url('/assets/TemplateAdmin/plugins/toaster/toastr.min.css')}}" rel="stylesheet" />
+  <link href="{{url('/assets/TemplateAdmin/plugins/nprogress/nprogress.css')}}" rel="stylesheet" />
+  <link href="{{url('/assets/TemplateAdmin/plugins/flag-icons/css/flag-icon.min.css')}}" rel="stylesheet"/>
+  <link href="{{url('/assets/TemplateAdmin/plugins/jvectormap/jquery-jvectormap-2.0.3.css')}}" rel="stylesheet" />
+  <link href="{{url('/assets/TemplateAdmin/plugins/ladda/ladda.min.css')}}" rel="stylesheet" />
+  <link href="{{url('/assets/TemplateAdmin/plugins/select2/css/select2.min.css')}}" rel="stylesheet" />
+  <link href="{{url('/assets/TemplateAdmin/plugins/daterangepicker/daterangepicker.css')}}" rel="stylesheet" />
+  <!-- SLEEK CSS -->
+  <link id="sleek-css" rel="stylesheet" href="{{url('/assets/TemplateAdmin/css/sleek.css')}}" />
+  <!-- FAVICON -->
+  <link href="{{url('/assets/TemplateAdmin/img/favicon.png')}}" rel="shortcut icon" />
+  <!--
+    HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
+  -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <script src="{{url('/assets/TemplateAdmin/plugins/nprogress/nprogress.js')}}"></script>
 </head>
 
-<body>
-<!-- header -->
-<div class="agileits_header">
-	<div class="w3l_offers">
-		<a href="{{url('/login')}}">Our Popular Products !!</a>
-	</div>
-	<div class="product_list_header">
-		<a href="{{url('/login')}}" class="button-cart">
-			<span>View My Cart</span>
-		</a>
-	</div>
-	<div class="clearfix"> </div>
-</div>
+</head>
+  <body class="bg-light-gray" id="body">
+      <div class="container d-flex flex-column justify-content-between vh-100">
+      <div class="row justify-content-center mt-5">
+        <div class="col-xl-5 col-lg-6 col-md-10">
+          <div class="card">
+            <div class="card-header bg-primary">
+              <div class="app-brand">
+                <a href="/index.html">
+                  <svg class="brand-icon" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" width="30" height="33"
+                    viewBox="0 0 30 33">
+                    <g fill="none" fill-rule="evenodd">
+                      <path class="logo-fill-blue" fill="#7DBCFF" d="M0 4v25l8 4V0zM22 4v25l8 4V0z" />
+                      <path class="logo-fill-white" fill="#FFF" d="M11 4v25l8 4V0z" />
+                    </g>
+                  </svg>
+                  <span class="brand-name">Admin Panel</span>
+                </a>
+              </div>
+            </div>
+            <div class="card-body p-5">
 
-<div class="logo_products">
-	<div class="container">
-		<div class="w3ls_logo_products_left">
-			<h1><a href="{{url('/')}}"><span>Grocery</span> Store</a></h1>
-		</div>
-		<div class="w3ls_logo_products_left1" id="menu">
-			<ul class="special_items">
-				<li><a href="{{url('/')}}">Home</a><i>/</i></li>
-				<li><a href="{{url('/product')}}">Product</a><i>/</i></li>
-				<li><a href="#footer">About Us</a></li>
-			</ul>
-		</div>
-		<div class="clearfix"> </div>
-	</div>
-</div>
-<!-- //header -->
-
-<!-- banner -->
-<div class="banner">
-
-	<div class="col-md-12 w3l_banner_nav_right ">
-		<!-- login -->
-		<div class="w3_login">
-
-			<div class="w3_login_module">
-				<div class="module form-module">
-				  <div class="form">
-				  </div>
-				  <div class="form shadow">
-					<h2>LOG IN</h2>
-					<form action="#" method="post">
-					  <input type="text" name="Username" placeholder="Username" required=" ">
-					  <input type="password" name="Password" placeholder="Password" required=" ">
-					  <input type="submit" value="Login" class="mt-5">
-					</form>
-				  </div>
-				</div>
-			</div>
-		</div>
-	<!-- //login -->
-		<!-- //about -->
-	</div>
-	<div class="clearfix"></div>
-</div>
-<!-- //banner -->
-
-<!-- footer -->
-<div class="footer" id="footer">
-	<div class="container">
-		<div class="col-md-4 w3_footer_grid ">
-			<h3>ABOUT US</h3>
-			<h4>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Sit, earum officia, a ut minus nesciunt itaque ab deserunt pariatur
-				ipsam culpa sunt vitae vero temporibus minima quasi iusto maxime quam?
-			</h4>
-			<div class="w3_footer_grid_bottom">
-				<ul class="agileits_social_icons">
-					<li><a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-					<li><a href="#" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-					<li><a href="#" class="google"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-					<li><a href="#" class="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-				</ul>
-			</div>
-		</div>
-		<div class="col-md-4 w3_footer_grid">
-			<h3>CONTACT</h3>
-			<ul class="contact" style='list-style-type: none;'>
-				<li><i class="fa fa-map-marker"></i> <span> Jl. Akordion, Tunggulwulung, Kec. Lowokwaru, Kota Malang, Jawa Timur 65143</span> </li>
-				<li><i class="fa fa-phone"></i> <span> +6281-007-776-667</span></li>
-				<li><i class="fa fa-envelope"></i> <span> abc@gmail.com</span> </li>
-			</ul>
-		</div>
-		<div class="col-md-4 w3_footer_grid">
-			<h3>what in stores</h3>
-			<ul class="w3_footer_grid_list">
-				<li><a href="{{url('/login')}}">Home</a></li>
-				<li><a href="{{url('/login')}}">Products</a></li>
-				<li><a href="#about">About</a></li>
-			</ul>
-		</div>
-		<div class="clearfix"> </div>
-		<div class="wthree_footer_copy">
-			<p>© 2016 Grocery Store. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
-		</div>
-	</div>
-</div>
-<!-- //footer -->
-
-<script src="{{url('/assets/TemplateUser/js/bootstrap.min.js')}}"></script>
-<script type="text/javascript" src="{{url('/assets/TemplateUser/js/index.js')}}"> </script>
+              <h4 class="text-dark mb-5">
+               <center> Log In</center></h4>
+              <form action="/index.html">
+                <div class="row">
+                  <div class="form-group col-md-12 mb-4">
+                    <input type="email" class="form-control input-lg" id="email" aria-describedby="emailHelp" placeholder="Username">
+                  </div>
+                  <div class="form-group col-md-12 ">
+                    <input type="password" class="form-control input-lg" id="password" placeholder="Password">
+                  </div>
+                  <div class="col-md-12">
+                    <button type="submit" class="btn btn-lg btn-primary btn-block mb-4">Sign In</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copyright pl-0">
+        <p class="text-center">&copy; 2018 Copyright Sleek Dashboard Bootstrap Template by
+          <a class="text-primary" href="#" target="_blank">Abdus</a>.
+        </p>
+      </div>
+    </div>
 </body>
 </html>
