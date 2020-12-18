36e154f6b62c79f288b497cb19a0eb9ea081fe57
wahono <wahono.gusty12@gmail.com>
2020-12-18 17:19:17 +0700

    fix conflict again

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,19 +9,14 @@ use App\Models\Kategori;
 
 class AdminController extends Controller
 {
-<<<<<<< HEAD
     public function __construct()
     {
         $this->middleware('auth');
-=======
-    public function Dashboard(){
-        $kategori = Kategori::all();
-        $produk = Produk::all();
-        return view('admin/dashboard/dashboard', compact('produk','kategori'));
->>>>>>> 252fa6947f128c2911aa4ed837caf8d175d91845
     }
 
     public function index(){
-        return view('admin/dashboard/dashboard');
+        $kategori = Kategori::all();
+        $produk = Produk::all();
+        return view('admin/dashboard/dashboard', compact('produk','kategori'));
     }
 }
